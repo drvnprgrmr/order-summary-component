78df17a15507ad61375dba2282f780045304be50
drvnprgrmr <ekunweibukun159357@gmail.com>
2023-07-22 19:06:51 +0100

    add html structure

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,20 +1,35 @@
 import styles from "./page.module.css";
 
+
 export default function Home() {
     return (
         <main>
-            Order Summary You can now listen to millions of songs, audiobooks,
-            and podcasts on any device anywhere you like! Annual Plan
-            $59.99/year Change Proceed to Payment Cancel Order
+            <div className="hero-img"></div>
+            <div>
+                <h1>Order Summary</h1>
+                <p>
+                    You can now listen to millions of songs, audiobooks, and
+                    podcasts on any device anywhere you like!
+                </p>
+                <div>
+                    <img src="./images/illustration-hero.svg" alt="hero img" />
+                    <p>Annual Plan</p>
+                    <p>$59.99/year</p>
+                    <p>Change</p>
+                </div>
+                <button>Proceed to Payment</button>
+                <button>Cancel Order</button>
+            </div>
+
             <div className={styles.attribution}>
-                Challenge by{" "}
+                Challenge by
                 <a
                     href="https://www.frontendmentor.io?ref=challenge"
                     target="_blank"
                 >
                     Frontend Mentor
                 </a>
-                . Coded by <a href="#">Your Name Here</a>.
+                . Coded by <a href="https://github.com/drvnprgrmr">drvnprgrmr</a>.
             </div>
         </main>
     );
